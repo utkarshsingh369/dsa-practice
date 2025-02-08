77b68a4979580db94c4d3dc26f67c1a4bf4a443c
utkarshsingh369 <utkarsh060902@gmail.com>
2025-02-08 20:40:44 +0530

    factorial through recursion

---
+++ recursion/Factorial.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class Factorial {
+    private static int factorial(int n){
+        if(n==1){
+            return 1;
+        }
+        return factorial(n-1)* n;
+        
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int n =sc.nextInt();
+        System.out.println(factorial(n));
+    }
+}
